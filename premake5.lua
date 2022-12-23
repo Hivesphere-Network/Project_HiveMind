18c556397c131b5993d95a441ddf72e8f86c6fea
Buddy365 <pramudithapothuwila36@gmail.com>
2022-12-23 11:32:43 +0530

    premake5.lua added for BaseAgent

--- premake5.lua
+++ premake5.lua
@@ -1,19 +1,4 @@
 workspace "Project HiveMind"
     configurations { "Debug", "Release" }
 
-project "BaseAgent"
-    kind "ConsoleApp"
-    language "C++"       
-    cppdialect "C++17"
-    staticruntime "on"
-    targetdir "bin/%{cfg.buildcfg}"
-    
-	files {"src/**.h", "src/**.cpp"}
-
-    ilter "configurations:Debug"
-      defines { "DEBUG" }
-      symbols "On"
-
-   filter "configurations:Release"
-      defines { "NDEBUG" }
-      optimize "On"
+include "BaseAgent"
