33ed1596f387d77af5e1e9308778d68691cdf46f
Buddy365 <pramudithapothuwila36@gmail.com>
2023-01-03 12:31:32 +0530

    ThreadWorker created

---
+++ BaseAgent/src/Worker.cpp
@@ -0,0 +1,58 @@
+﻿#include "Worker.h"
+
+Worker::Worker(const Worker&)
+{
+	
+}
+
+Worker::~Worker()
+{
+	this->abort_and_join();
+}
+
+bool Worker::run()
+{
+	m_running.store(true);
+	
+	while(m_abort_requested.load() == false)
+	{
+		try
+		{
+			m_thread = std::thread(&Worker::run_func, this);
+		}
+		catch(std::runtime_error& error)
+		{
+			
+		}
+		catch(...)
+		{
+				
+		}
+	}
+
+	m_running.store(false);
+}
+
+bool Worker::stop()
+{
+	this->abort_and_join();
+}
+
+bool Worker::is_running()
+{
+	
+}
+
+bool Worker::run_func()
+{
+	
+}
+
+bool Worker::abort_and_join()
+{
+	m_abort_requested.store(true);
+	if(m_thread.joinable())
+	{
+		m_thread.join();
+	}
+}
---
+++ BaseAgent/src/Worker.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <thread>
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <stdexcept>
+
+class Worker
+{
+private:
+	std::thread m_thread;
+	std::atomic_bool m_running;
+	std::atomic_bool m_abort_requested;
+
+public:
+	Worker(const Worker&);
+	~Worker();
+	bool run();
+	bool stop();
+	bool is_running();
+
+private:
+	bool run_func();
+	bool abort_and_join();
+	
+};
