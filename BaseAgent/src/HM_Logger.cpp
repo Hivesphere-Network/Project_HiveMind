ee01b02bdb81de31f47ecd51023d7309198c2383
Buddy365 <pramudithapothuwila36@gmail.com>
2023-01-05 14:11:31 +0530

    HM_Logger basic functions implemented

--- BaseAgent/src/HM_Logger.cpp
+++ BaseAgent/src/HM_Logger.cpp
@@ -1,2 +1,23 @@
 ﻿#include "HM_Logger.h"
 
+HM_Logger::HM_Logger()
+{
+	m_log_file = "logs/base_log.txt";
+	m_file = new HM_File(m_log_file.c_str(), std::ios::in | std::ios::out | std::ios::app);
+	m_log_level = LOG_LEVEL::LOG_LEVEL_INFO;
+}
+
+HM_Logger::~HM_Logger()
+{
+	delete m_file;
+}
+
+void HM_Logger::info(const std::string& log_string)
+{
+	m_log_queue.append("[INFO] " + log_string + "\n");
+}
+
+void HM_Logger::debug(const std::string& log_string)
+{
+	m_log_queue.append("[DEBUG] " + log_string + "\n");
+}
--- BaseAgent/src/HM_Logger.h
+++ BaseAgent/src/HM_Logger.h
@@ -1,8 +1,31 @@
 ﻿#pragma once
 
+#include <ctime>
+#include <string>
+
+#include "HM_File.h"
+
+enum class LOG_LEVEL
+{
+	LOG_LEVEL_DEBUG,
+	LOG_LEVEL_INFO,
+	LOG_LEVEL_WARNING,
+	LOG_LEVEL_ERROR
+};
+
 class HM_Logger
 {
+private:
+	HM_File* m_file;
+	LOG_LEVEL m_log_level;
+	std::string m_log_file;
+
+	std::string m_log_queue;
+	
 public:
-	
-	
+	HM_Logger();
+	~HM_Logger();
+
+	void info(const std::string& log_string);
+	void debug(const std::string& log_string);
 };
